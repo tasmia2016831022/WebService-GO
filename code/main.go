1819d70c6343d2a662cd5c311dde66cc1ac44fa3
Tasmia Alamgir <tasmia.alamgir@pathao.com>
2020-04-25 05:06:17 +0600

    readme added

--- code/main.go
+++ code/main.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("Helllo Gophers !!")
+
+	// initialize integer 
+	var i int
+	i = 90
+	fmt.Println(i)
+
+	//initialize float ( 32 - float32, 64 - float64)
+	var f float32 = 56
+	fmt.Println(f)
+
+	//implying according to initalization (type)
+	var firstName = "Tasmia"
+	fmt.Println(firstName)
+
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+for running 
+```cd code ```
+initialize go
+``` go init <dependency module> ```
+``` go run <filename> ```
+or,
+``` go run <dependency module> ```
